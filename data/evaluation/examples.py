fbb28d70f2a9cb6a984a7b9fc3c502c85a5c00e8
Fabian <f.boeger@mail.de>
2026-04-08 19:11:44 +0200

    added example dataset

---
+++ data/evaluation/examples.py
@@ -0,0 +1,22 @@
+examples = [
+    {
+        "inputs": {"question": "Wieviele Straßen bekommt jeder Spieler am Anfang?"},
+        "outputs": {"answer": "15 Straßen"},
+    },
+    {
+        "inputs": {"question": "Wieviele Siedlungen, Städte und Straßen bekommt jeder Spieler am Anfang?"},
+        "outputs": {"answer": "5 Siedlungen, 4 Städte und 15 Straßen"},
+    },
+    {
+        "inputs": {"question": "Wer beginnt das Spiel?"},
+        "outputs": {"answer": "Es beginnt der älteste Spieler."},
+    },
+        {
+        "inputs": {"question": "Was erfordert eine Siedlung?"},
+        "outputs": {"answer": "Lehm, Holz, Wolle, Getreide"},
+    },
+        {
+        "inputs": {"question": "Was passiert beim Würfeln einer 7?"},
+        "outputs": {"answer": "Das Würfeln einer 7 aktiviert den Räuber. In diesem Zug bekommt niemand Rohstoffe und wer mehr als 7 Karten hat, muss die Hälfte seiner Karten ablegen."},
+    },
+]
